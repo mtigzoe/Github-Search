4cef6d5ae602673dc8af2561e5f6e5a4f922a710
mtigzoe <mred5248@gmail.com>
2024-02-24 16:13:43 -0500

    Edit the file.

--- github-search.py
+++ github-search.py
@@ -7,17 +7,19 @@ def search_github_repos(query):
     data = response.json()
 
     # Process the search results (e.g., extract repo names, descriptions, etc.)
-    count = 0
-    for item in data.get("items", []):
-        count += 1
-        repo_url = item["svn_url"]
-        repo_name = item["name"]
-        repo_description = item["description"]
-        print(f"URL: {repo_url}")
-        print(f"Repository: {repo_name}")
-        print(f"Description: {repo_description}\n")
-        time.sleep(1)
-    print(f"Total repos found: {count}")
+    with open("results.txt", "w", encoding="utf-8") as fout:
+
+        count = 0
+        for item in data.get("items", []):
+            count += 1
+            repo_url = item["svn_url"]
+            repo_name = item["full_name"]
+            repo_description = item["description"]
+            fout.write(str(f"URL: {repo_url}\n"))
+            fout.write(str(f"Repository: {repo_name}\n"))
+            fout.write(str(f"Description: {repo_description}\n\n"))
+            time.sleep(1)
+        print(f"Total repos found: {count}")
 
 search_query = input("Search: ") 
 
